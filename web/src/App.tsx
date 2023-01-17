3d399867bf20989f93271ce10985bfd18aef1b2a
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-16 21:30:31 -0300

    web/front prebuild

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Habit } from './components/Habit'
+
+function App() {
+  return (
+    <>
+      <Habit completed={5} />
+      <h1>HELLO EVERYONE</h1>
+    </>
+  )
+}
+
+export default App
