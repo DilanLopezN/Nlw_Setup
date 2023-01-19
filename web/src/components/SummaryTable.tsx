5e043be131c578d83ee91fa003146332cfe6edbe
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-18 22:53:02 -0300

    fill summarytable with allowed habit days

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 import { HabitDay } from './HabitDay'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -18,7 +24,16 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
+        {summaryDates.map(date => {
+          return <HabitDay />
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map(() => {
+            return (
+              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-60 cursor-not-allowed"></div>
+            )
+          })}
       </div>
     </div>
   )
