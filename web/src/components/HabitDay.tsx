e05724f35decd0f6b13b8db9471954a0acb2f502
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-18 22:35:08 -0300

    summary component

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,7 +1,7 @@
 interface HabitProps {
   completed: number
 }
-export function Habit(props: HabitProps) {
+export function HabitDay(props: HabitProps) {
   return (
     <>
       <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
