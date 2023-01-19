a190c74ce13f8ce5b9bb835d4d10b5fffbdd6b30
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-19 12:15:54 -0300

    reuse utils from web

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from 'react-native'
 import { HabitDay, DAY_SIZE } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const datesFromYearStart = generateDatesFromYearBeginning()
+
 export default function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
