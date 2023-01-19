ccd8431b69ca754985fe4de9ce2c47ee91898ff3
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-18 22:58:22 -0300

    finish layout web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -31,7 +31,7 @@ export function SummaryTable() {
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map(() => {
             return (
-              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-60 cursor-not-allowed"></div>
+              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-60 cursor-not-allowed" />
             )
           })}
       </div>
